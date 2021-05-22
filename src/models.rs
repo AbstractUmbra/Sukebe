58f48235df81556982d2fa637ae0d98f42ea4905
Alex Nørgaard <Umbra@AbstractUmbra.xyz>
2021-05-22 16:14:31 +0100

    api gives a string SOMETIMES. This works now
    Co-authored-by: Nadir Chowdhury <chowdhurynadir0@outlook.com>

--- src/models.rs
+++ src/models.rs
@@ -5,6 +5,7 @@ use chrono::{
     DateTime, Utc,
 };
 use serde::{de, Deserialize, Deserializer};
+use serde_json::Value;
 use std::fmt;
 use structopt::StructOpt;
 
@@ -97,17 +98,41 @@ pub struct Title {
     pub pretty: String,
 }
 
-fn to_u32<'de, D>(value: D) -> Result<u32, D::Error>
+struct ToU32Visitor;
+
+impl<'de> de::Visitor<'de> for ToU32Visitor {
+    type Value = u32;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a String or Integer")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        serde_json::from_str(v).map_err(E::custom)
+    }
+
+    fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        Ok(v as u32)
+    }
+}
+
+fn to_u32<'de, D>(deserializer: D) -> Result<u32, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let s: &str = Deserialize::deserialize(value)?;
-    s.parse().map_err(de::Error::custom)
+    deserializer.deserialize_any(ToU32Visitor)
 }
 
 /// The full API response per Gallery.
 #[derive(Deserialize, Debug)]
 pub struct Doujin {
+    #[serde(deserialize_with = "to_u32")]
     pub id: u32,
     pub images: Images,
     #[serde(deserialize_with = "to_u32")]
@@ -121,6 +146,13 @@ pub struct Doujin {
     pub upload_date: DateTime<Utc>,
 }
 
+#[derive(Deserialize, Debug)]
+pub struct DoujinSearch {
+    pub result: Vec<Doujin>,
+    pub num_pages: Value,
+    pub per_page: Value,
+}
+
 impl Doujin {
     pub async fn new(gallery_id: u32) -> Result<Self> {
         let url = format!("https://nhentai.net/api/gallery/{}", gallery_id);
@@ -132,6 +164,56 @@ impl Doujin {
             .context("Invalid API response")?;
 
         Ok(result)
+    }
+
+    pub async fn search(query: &str) -> Result<Vec<Self>> {
+        let url = format!("https://nhentai.net/api/galleries/search");
+        let client = reqwest::Client::new();
+
+        let response = client
+            .get(&url)
+            .query(&[("query", query)])
+            .send()
+            .await
+            .with_context(|| format!("Could not perform search with query `{}", query))?
+            .json::<DoujinSearch>()
+            .await
+            .with_context(|| format!("Could not parse response from url `{}`", &url))?;
+
+        Ok(response.result)
+    }
+
+    pub async fn from_tag(tag_id: u32) -> Result<Vec<Self>> {
+        let url = format!("https://nhentai.net/api/galleries/tagged");
+        let client = reqwest::Client::new();
+
+        let response = client
+            .get(&url)
+            .query(&[("tag_id", &tag_id)])
+            .send()
+            .await
+            .with_context(|| format!("Could not search tags with id `{}", &tag_id))?
+            .json::<DoujinSearch>()
+            .await
+            .with_context(|| format!("Could not parse response from url `{}`", &url))?;
+
+        Ok(response.result)
+    }
+
+    pub async fn from_alike(doujin_id: u32) -> Result<Vec<Self>> {
+        let url = format!("https://nhentai.net/api/gallery/{}/related", doujin_id);
+        let client = reqwest::Client::new();
+
+        let response = client
+            .get(&url)
+            .send()
+            .await
+            .with_context(|| format!("Could not search related doujin with id `{}", &doujin_id))?
+            .json::<DoujinSearch>()
+            .await
+            .with_context(|| format!("Could not parse response from url `{}`", &url))?;
+
+        Ok(response.result)
     }
 
     pub fn pretty_date(&self) -> DelayedFormat<StrftimeItems> {
